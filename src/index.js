3084ed36b38c507afb2be5d67b9823c38de7e25e
Student <example@example.com>
2019-03-11 15:02:16 -0700

    complete Simon.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,88 @@
 import './sass/styles.scss';
 import $ from 'jquery';
-import { randomColor } from './simon.js';
+import {colors, empty2, randomColor, replayColors, isEqual, passed } from './simon.js';
+
+export var selectColors = [];
+
+$(document).ready(function() {
 
 
 
 
-$("#start-game").click(function() {
-  $("#start-game").hide();
-  $(".buttons").show();
-  randomColor();
+  function eraseAll() {
+    $(".player-input").replaceWith("<div class='player-input'>");
+  }
+
+
+
+  $("#start-game").click(function(event) {
+    $("#start-game").hide();
+    randomColor();
+    setTimeout(function() {
+      $(".simons-colors").css("background-color", "white");
+      $(".buttons").show();
+      $('.submit').show();
+
+    }, 2000);
+    event.preventDefault();
+  });
+
+  $("#next-round").click(function(event) {
+    isEqual();
+    if(passed == true) {
+      $("#next-round").hide();
+      $(".buttons").hide();
+      replayColors();
+      event.preventDefault();
+      selectColors = [];
+      eraseAll();
+    } else {
+
+      empty2();
+      eraseAll();
+      $(".buttons").hide();
+      $("#next-round").hide();
+      $(".player-input").append("<h1>GIT GUD, SCRUB.</h1>");
+      setTimeout(function() {
+          window.location.href = "./index.html";
+
+      }, 2000);
+    }
+  });
+
+  $("#red").click(function() {
+
+    selectColors.push("Red");
+    $(".player-input").append("<p>Red</p>");
+
+  });
+
+  $("#orange").click(function() {
+    selectColors.push("Orange");
+    $(".player-input").append("<p>Orange</p>");
+
+  });
+
+  $("#yellow").click(function() {
+    selectColors.push("Yellow");
+    $(".player-input").append("<p>Yellow</p>");
+  });
+
+  $("#green").click(function() {
+    selectColors.push("Green");
+    $(".player-input").append("<p>Green</p>");
+  });
+
+  $("#blue").click(function() {
+
+
+    selectColors.push("Blue");
+    $(".player-input").append("<p>Blue</p>");
+  });
+
+  $("#violet").click(function() {
+    selectColors.push("Violet");
+    $(".player-input").append("<p>Violet</p>");
+
+  });
 });
--- src/simon.js
+++ src/simon.js
@@ -1,28 +1,97 @@
 import $ from 'jquery';
+import { selectColors } from './index.js';
 
+export var colors = [];
+export var passed;
 
 $(".buttons").hide();
+$('.submit').hide();
+
+export function empty2() {
+  colors = [];
+}
 
 export function randomColor() {
-  debugger;
 
   var random = Math.floor((Math.random() * 6) + 1);
   if (random == 1) {
     $(".simons-colors").css("background-color", "red");
+    colors.push("Red");
   }
   if (random == 2) {
     $(".simons-colors").css("background-color", "orange");
+    colors.push("Orange");
   }
   if (random == 3) {
     $(".simons-colors").css("background-color", "yellow");
+    colors.push("Yellow");
   }
   if (random == 4) {
     $(".simons-colors").css("background-color", "green");
+    colors.push("Green");
   }
   if (random == 5) {
     $(".simons-colors").css("background-color", "blue");
+    colors.push("Blue");
   }
   if (random == 6) {
     $(".simons-colors").css("background-color", "violet");
+    colors.push("Violet");
+  }
+}
+
+
+
+
+
+export function replayColors() {
+  var length = colors.length;
+  var i;
+  for (i = 0; i < length; i++) {
+    var color = colors[i];
+    playColor(color, i);
+
+  }
+  setTimeout(function() {
+    if(i == length) {
+      randomColor();
+    }
+  }, 2000 * i);
+  setTimeout(function() {
+    if(i == length) {
+      $(".simons-colors").css("background-color", "white");
+      $("#next-round").show();
+      $(".buttons").show();
+    }
+  }, 2000 * i + 1000);
+}
+
+
+
+function playColor(color, i) {
+  setTimeout(function() {
+    $(".simons-colors").css("background-color", color);
+  }, 2000 * i);
+  setTimeout(function() {
+    $(".simons-colors").css("background-color", "white");
+
+  }, 2000 * i + 1000);
+}
+
+
+
+
+export function isEqual() {
+  var isCorrect = 0;
+  var length = colors.length;
+  for(var i = 0; i < length; i++) {
+    if(selectColors[i] == colors[i]) {
+      isCorrect = isCorrect + 1;
+    }
+  }
+  if(isCorrect == length) {
+    passed = true;
+  } else {
+    passed = false;
   }
 }
